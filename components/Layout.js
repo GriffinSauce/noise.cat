56fe2b443dc91275904f878b93d0f03c95feb51b
GriffinSauce <jorisjgriffioen@gmail.com>
2019-11-28 21:50:38 +0100

    Quick fonts for the eyes

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,23 @@
 import Head from 'next/head';
 
+// TODO: Review font usage and trim
 const Layout = ({ children }) => {
   return (
     <>
       <Head>
         <title>noise.cat</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <main>{children}</main>
       <style jsx>{`
         :global(body) {
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-            Helvetica, sans-serif;
+          font-family: 'Open Sans', sans-serif;
+          font-size: 13px;
         }
         main {
           padding-bottom: 50px;
