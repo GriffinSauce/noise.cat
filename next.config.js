cb2cf685f6fc49ad3006f327b22f85ccca6c81e0
GriffinSauce <jorisjgriffioen@gmail.com>
2020-01-05 21:21:46 +0100

    Prevent crash by removing leftovers

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
-const { parsed: localEnv } = require('dotenv').config();
-const webpack = require('webpack');
-
-console.info('localEnv.AUTH0_COOKIE_SECRET', localEnv.AUTH0_COOKIE_SECRET);
 console.info(
   'process.env.AUTH0_COOKIE_SECRET',
   process.env.AUTH0_COOKIE_SECRET,
 );
 
 module.exports = {
-  webpack(config, options) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-    return config;
-  },
   env: {
     AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
     AUTH0_SECRET: process.env.AUTH0_SECRET,
