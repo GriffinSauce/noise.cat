1be60d8895704d58127b9a222ab08d698c4b3036
GriffinSauce <jorisjgriffioen@gmail.com>
2020-01-05 20:37:26 +0100

    Provide vars to build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-require('dotenv').config();
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
+
 module.exports = {
+  webpack(config, options) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    return config;
+  },
   env: {
     AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
     AUTH0_SECRET: process.env.AUTH0_SECRET,
