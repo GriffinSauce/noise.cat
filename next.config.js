977d67859b40d78ecf879f29774f25a508dba39c
GriffinSauce <jorisjgriffioen@gmail.com>
2020-03-10 23:17:16 +0100

    Point now route to it + some basic SW config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,27 @@ require('dotenv').config();
 const withOffline = require('next-offline');
 
 module.exports = withOffline({
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
   workboxOpts: {
     swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
   },
   env: {
     AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
