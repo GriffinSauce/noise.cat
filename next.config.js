30927b7d6ed93e67b08e1e39f51ef2d6a729440f
GriffinSauce <jorisjgriffioen@gmail.com>
2020-03-14 19:01:09 +0100

    Env vars should not be in build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const withOffline = require('next-offline');
 
 module.exports = withOffline({
@@ -23,13 +22,5 @@ module.exports = withOffline({
         },
       },
     ],
-  },
-  env: {
-    AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
-    AUTH0_SECRET: process.env.AUTH0_SECRET,
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    AUTH0_COOKIE_SECRET: process.env.AUTH0_COOKIE_SECRET,
-    CS_API_URL: process.env.CS_API_URL,
-    LA_NOTION_TOKEN: process.env.LA_NOTION_TOKEN,
   },
 });
