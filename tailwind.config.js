45bec54033902a1f7e0c0edde2cc0cb449d7b2da
GriffinSauce <jorisjgriffioen@gmail.com>
2020-05-08 22:33:36 +0200

    Typescriiiiiipt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    content: ['./components/**/*.js', './pages/**/*.js'],
+    content: ['./components/**/*.tsx', './pages/**/*.tsx', './styles/**/*.css'],
   },
   theme: {
     extend: {
