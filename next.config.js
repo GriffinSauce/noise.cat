f99ed6c33b6dcbec6f04813d70f9d50875800290
GriffinSauce <jorisjgriffioen@gmail.com>
2020-10-01 23:19:21 +0200

    Use new rewrites and headers config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withOffline = require('next-offline');
 
-module.exports = withOffline({
+const offlineConfig = {
   target: 'serverless',
   transformManifest: (manifest) => ['/'].concat(manifest), // add the homepage to the cache
   workboxOpts: {
@@ -23,5 +23,30 @@ module.exports = withOffline({
         },
       },
     ],
+  },
+};
+
+module.exports = withOffline({
+  ...offlineConfig,
+  async rewrites() {
+    return [
+      {
+        source: '/service-worker.js',
+        destination: '/_next/static/service-worker.js',
+      },
+    ];
+  },
+  async headers() {
+    return [
+      { source: '/user/:id', headers: [{ key: 'x-path', value: 'hi' }] },
+
+      {
+        source: '/service-worker.js',
+        headers: [
+          { key: 'cache-control', value: 'public, max-age=43200, immutable' },
+          { key: 'Service-Worker-Allowed', value: '/' },
+        ],
+      },
+    ];
   },
 });
