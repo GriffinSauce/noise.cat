61feced20f326a408bf3252101d1af40011f8b25
Joris Griffioen <jorisjgriffioen@gmail.com>
2022-03-29 09:17:19 +0200

    Add name components and last synced time

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -4,7 +4,10 @@ import { prisma } from 'utils/prisma';
 const afterCallback: AfterCallback = async (_req, _res, session, _state) => {
   const userData = {
     externalId: session.user.sub as string,
+    synced: new Date(),
     name: session.user.name as string,
+    givenName: session.user.given_name,
+    familyName: session.user.family_name,
     email: session.user.email as string,
     ...(session.user.picture
       ? {
